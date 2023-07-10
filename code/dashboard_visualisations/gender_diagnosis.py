47ce36c0d9f9fefbd5cb83e98cabc9a15549d5a0
jamesruffle <j.ruffle@ucl.ac.uk>
2023-07-10 08:27:33 +0100

    push code

---
+++ code/dashboard_visualisations/gender_diagnosis.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+# Sample data
+df = pd.read_csv("/Users/sam/Desktop/james-interactive-graph/code/simulated_data/simulated_data.csv")
+
+# Group the DataFrame by Gender and Diagnosis and count the occurrences
+grouped_df = df.groupby(['Gender', 'Diagnosis']).size().reset_index(name='Count')
+
+# Define colors for the donut plot
+colors = ['#FFC0CB', '#ADD8E6', '#98FB98', '#FFD700']
+
+# Create the donut plot
+fig = go.Figure(data=[go.Pie(
+    labels=grouped_df['Gender'],
+    values=grouped_df['Count'],
+    hole=0.6,
+    marker=dict(colors=colors),
+    hovertemplate='Gender: %{label}<br>Count: %{value}',
+)])
+
+# Set layout properties
+fig.update_layout(
+    title='Gender vs Diagnosis',
+    plot_bgcolor='white',
+    paper_bgcolor='white',
+    showlegend=True,
+    legend=dict(orientation='v', yanchor='middle', y=0.5, xanchor='right', x=1),
+    #legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1),
+    margin=dict(l=5, r=5, t=50, b=50)
+)
+
+# Save the plot as a high-quality PNG image
+fig.write_image('/Users/sam/Desktop/james-interactive-graph/code/geospatial/gender_vs_diagnosis.png', format='png', width=600, height=600, scale=2)
